1c44046ca253df827f80d7f7a00695638e0c8499
Shivani Mishra <shivanimishra@Bhanus-MacBook-Pro-2.local>
2025-08-27 12:27:22 +0200

    init

---
+++ giphy-challenge/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Giphy Challenge</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
