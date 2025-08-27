98af5ef630ed874581ca7bf9ad818f9bf97a964f
Shivani Mishra <shivanimishra@Bhanus-MacBook-Pro-2.local>
2025-08-27 12:53:23 +0200

    adding typescript

--- giphy-challenge/vite.config.ts
+++ giphy-challenge/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change this to any port you prefer
+    port: 3000,
   },
 })
